f8bd3d7560e599d6ae288860bfb3360461860853
morshedalam3 <morshedhasan1969@gmail.com>
2021-08-09 15:54:18 +0600

    add

--- Backend/index.js
+++ Backend/index.js
@@ -15,8 +15,8 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT|| 8000;
-
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+CONNECTION_URL = 'mongodb+srv://notebookdb:IvWnQzBWOMsFhC0K@cluster0.ossg7.mongodb.net/<dbnote>?retryWrites=true&w=majority'
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
